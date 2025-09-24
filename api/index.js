8edc713956f17c4f2a4ef3c365cb995c5c7d8482
Surya <157699321+Surya-palanisamy@users.noreply.github.com>
2025-09-24 13:56:32 +0530

    Simplify response in cookie handler
    Removed extra text from the response body.

--- api/index.js
+++ api/index.js
@@ -3,12 +3,10 @@ import cookie from "cookie";
 export default function handler(req, res) {
   // Set cookie
   res.setHeader("Set-Cookie", cookie.serialize("role", "guest", { path: "/" }));
-
   res.status(200).send(`
     <h2>Welcome to Cookie Monster</h2>
-    <p>Your role is: <b>guest</b></p>
-    <p>Hint: Sometimes cookies are more powerful than they look!</p>
-    <p> Not all roles are created equal… some hold the real power</p>
+    <p>Your role is: <b>guest</p>
   `);
 }
+
 
